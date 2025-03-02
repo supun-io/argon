c8bfbeb573c928d9402baf4de8554f7b8b3a5727
Supun Wimalasena <supun@hyvor.com>
2025-03-02 05:46:13 +0100

    remove lua

--- lua/notes.md
+++
@@ -1,1 +0,0 @@
-`lua` package resulted in 600kb build size (compared to 400kb for rust bare project)
--- lua/src/main.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let mut state = lua::State::new();
-    state.open_libs();
-    let _ = state.do_string("print('hello world!')");
-}
